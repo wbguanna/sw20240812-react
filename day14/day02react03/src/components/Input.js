6b2a3869ceffff1dc3455839d76466275d4cb03e
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-08-13 16:27:37 +0900

    day14  react-day02 props와state (todo입,출력)

--- day14/day02react03/src/components/Input.js
+++ day14/day02react03/src/components/Input.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
+
 export default ({ appendItem }) => {
   // 구조분해 할당 기호: 배열 [], 객체는 {}
+
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div className="input-group mb-3">
       <input
         type="text"
         className="form-control"
         placeholder="Something clever.."
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
       />
       <div className="input-group-append">
         <button
           onClick={() => {
-            appendItem("새 할일");
+            appendItem(inputValue);
+            setInputValue("");
           }}
           className="btn btn-primary"
           type="button"
