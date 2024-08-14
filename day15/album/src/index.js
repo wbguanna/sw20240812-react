4fc192c8ad7671eb45d9d166dabee16c4e692994
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-08-14 14:17:46 +0900

    day15 5교시 중 강사님 파일로교체(album)

--- day15/album/src/index.js
+++ day15/album/src/index.js
@@ -1,12 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "./w3.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App/>);
---
+++ day15/album/src/pages/Layout.js
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router-dom";
+import Heading from "../components/Heading";
+
+export default () =>{
+    return (<div>
+        <Heading />        
+        <Outlet />
+    </div>);
+}
