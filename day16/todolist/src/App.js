138b688bea1289069ad6dd69fb10db8f5a1117de
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-08-19 14:33:52 +0900

    day16 완성본으로 변경

--- day16/todolist/src/App.js
+++ day16/todolist/src/App.js
@@ -2,17 +2,22 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
+  // 전역변수를 state로 만들어 주어야 re rendering 된다.
+  // 구조분해 할당 = state변수, setter함수
   const [name, setName] = useState("Todo List");
   const [todoList, setTodoList] = useState([
-    { no: 1, title: "Buy milk", done: false },
-    { no: 2, title: "Bu milk", done: false },
-    { no: 3, title: "B mlk", done: false },
-    { no: 4, title: "uy ilk", done: false },
+    { no: 101, title: "공부하기", done: false },
+    { no: 102, title: "자바하기", done: true },
+    { no: 103, title: "리액트하기", done: false },
+    { no: 104, title: "스프링하기", done: false },
   ]);
-  const [noCnt, setNoCnt] = useState(5);
+  const [noCnt, setNoCnt] = useState(105);
+
   const [inputTitle, setInputTitle] = useState("");
+
   const onClickEvent = () => {
-    setTodoList([...todoList, { no: noCnt, title: "점심먹기", done: false }]);
+    // 기존 내용에 새 내용을 추가 해서 새 배열을 생성
+    setTodoList([...todoList, { no: noCnt, title: inputTitle, done: false }]);
     setNoCnt(noCnt + 1);
     setInputTitle("");
   };
@@ -22,122 +27,84 @@ const App = () => {
   };
 
   const onDelete = ({ no, title, done }) => {
-    console.dir(no);
     const newList = todoList.filter((todo) => {
       return todo.no != no;
     });
+    setTodoList(newList);
   };
+
   return (
     <div className="todoList">
       <div className="App-header">
         <h1>{name} App</h1>
-        {/* 중괄호 문자열 집어넣기가능 */}
       </div>
       <div className="input-title">
-        {/* <button onClick={onClickEvent}>Save</button> */}
-        {/* 중괄호로 이벤트 함수 추가 가능 */}
-        <div class="input-group mb-3">
-          <input
-            type="text"
-            onChange={onChangeTitle}
-            class="form-control"
-            placeholder="할일 입력"
-          />
-          <div class="input-group-append">
-            <button class="btn btn-success" type="submit">
-              Save
-            </button>
+        <div className="container" style={{ padding: "10px" }}>
+          <div className="input-group mb-3">
+            <input
+              value={inputTitle}
+              onChange={onChangeTitle}
+              type="text"
+              className="form-control"
+            />
+            <div className="input-group-append">
+              <button className="btn btn-success" onClick={onClickEvent}>
+                Save
+              </button>
+            </div>
           </div>
         </div>
       </div>
       <div className="list-body">
-        {/* 여기다 집어넣는것은 동적으로 집어넣는 방법은 아니다 */}
-        {/* ex) appendChild */}
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          {/* <tbody>
-            <tr>
-              <td>John</td>
-              <td>Doe</td>
-              <td>john@example.com</td>
-            </tr>
-            <tr>
-              <td>Mary</td>
-              <td>Moe</td>
-              <td>mary@example.com</td>
-            </tr>
-            <tr>
-              <td>July</td>
-              <td>Dooley</td>
-              <td>july@example.com</td>
-            </tr>
-          </tbody> */}
-          <tbody>
-            {todoList.map((item) => {
-              return (
-                <tr key={item.no}>
-                  {/* <td>
-                    <input type="checkbox" />
-                  </td>
-                  <td>
-                    <input type="text" />
-                  </td>
-                  <td className="btn-group">
-                    <button btn btn-primary>
-                      edit
-                    </button>
-                    <button btn btn-primary>
-                      delete
-                    </button>
-                  </td>
-                  <td>{item.title}</td> */}
-                  <td colSpan={3} style={{ padding: "0px" }}>
-                    <div className="input-group mb-3">
-                      <div className="input-group-prepend">
-                        <div className="input-group-text">
-                          <input type="checkbox" />
+        <div className="container">
+          <table className="table table-hover">
+            <thead>
+              <tr style={{ textAlign: "center" }}>
+                <th>Done</th>
+                <th>Title</th>
+                <th>Buttons</th>
+              </tr>
+            </thead>
+            <tbody>
+              {todoList.map((item) => {
+                return (
+                  <tr key={item.no}>
+                    <td colSpan={3} style={{ padding: "0px" }}>
+                      <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                          <div className="input-group-text">
+                            <input type="checkbox" />
+                          </div>
                         </div>
-                        <input type="" />
-                        <div className="btn-group">
-                          <button className="btn btn-primary">ok</button>
-
+                        <input
+                          type="text"
+                          className="form-control"
+                          readOnly
+                          value={item.title}
+                        />
+                        <div className="input-group-append">
+                          <button className="btn btn-primary" type="button">
+                            Edit
+                          </button>
                           <button
-                            onClick={(e) => {
-                              onDelete(e);
+                            onClick={() => {
+                              onDelete(item);
                             }}
                             className="btn btn-danger"
+                            type="button"
                           >
-                            cancel
+                            Delete
                           </button>
                         </div>
                       </div>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <ul>
-          {todoList.map((item) => {
-            // return item;
-            // return JSON.stringify(item);
-            return (
-              <li>
-                <input type="checkbox" />
-                <span>{item.title}</span>
-                <button>edit</button>
-                <button>X</button>
-              </li>
-            );
-          })}
-        </ul>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <ul></ul>
+        </div>
       </div>
     </div>
   );
