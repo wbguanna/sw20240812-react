320a5f42e85f89266227bba37682620e7b6b9556
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-08-20 14:36:30 +0900

    day17 server & todo_step03 axios로 리스트 받아와보기 (비동기통신)

--- day17/server/app.js
+++ day17/server/app.js
@@ -2,30 +2,40 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const parser = require("body-parser");
+const cors = require("cors");
 
 app.set("port", 5000);
 
 // static 미들 웨어
 app.use("/", express.static("public"));
 
+//  URL, port 등이 다른 클라이언트 요청허용 (가라다)
+app.use(cors());
+
+const todolist = [
+  { no: 101, todo: "일어나기", done: true },
+  { no: 102, todo: "먹기", done: false },
+  { no: 103, todo: "자기", done: true },
+];
+
 app.get("/todo", (req, res) => {
   // 목록출력
-  res.send();
+  res.send(todolist);
 });
 
 app.post("/todo", (req, res) => {
   // 할일 입력
-  res.send();
+  res.send(todolist);
 });
 
 app.put("/todo", (req, res) => {
   // 할일 수정
-  res.send();
+  res.send(todolist);
 });
 
 app.delete("/todo", (req, res) => {
   // 할일 삭제
-  res.send();
+  res.send(todolist);
 });
 
 const server = http.createServer(app);
--- day17/todolist_step03/src/App.js
+++ day17/todolist_step03/src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ItemRow from "./ItemRow";
 import { Input } from "./Input";
 import Output from "./Output";
+import axios from "axios";
 
 const App = () => {
   // 전역변수를 state로 만들어 주어야 re rendering 된다.
   // 구조분해 할당 = state변수, setter함수
   const [name, setName] = useState("Todo List");
-  const [todoList, setTodoLilst] = useState([
+  const [todoList, setTodoList] = useState([
     { no: 101, title: "공부하기", done: false },
     { no: 102, title: "자바하기", done: true },
     { no: 103, title: "리액트하기", done: false },
     { no: 104, title: "스프링하기", done: false },
   ]);
+
+  // useEffect() 훅 - 렌더링 되는 것과 비동기로 작동한다.
+  // 최초 한번만 실행됨.
+  // 훅은 콜백함수 내부에 포함 될 수 없다.
+  // 그래서 useEffect() 훅 내부에서 axios를 이용해서 처리.
+  useEffect(() => {
+    axios.get("http://localhost:5000/todo").then(function (res) {
+      console.log(res);
+      setTodoList(res["data"]);
+    });
+  });
   const [noCnt, setNoCnt] = useState(105);
 
   const [outputTitle, setOutputTtile] = useState("");
@@ -22,7 +34,7 @@ const App = () => {
 
   const onClickEvent = (inputTitle) => {
     // 기존 내용에 새 내용을 추가 해서 새 배열을 생성
-    setTodoLilst([...todoList, { no: noCnt, title: inputTitle, done: false }]);
+    setTodoList([...todoList, { no: noCnt, title: inputTitle, done: false }]);
     setNoCnt(noCnt + 1);
   };
 
@@ -30,7 +42,7 @@ const App = () => {
     const newList = todoList.filter((todo) => {
       return todo.no != no;
     });
-    setTodoLilst(newList);
+    setTodoList(newList);
   };
 
   const onDoneFlag = ({ no, title, done }) => {
@@ -40,7 +52,7 @@ const App = () => {
         newTodoList[idx].done = !done;
       }
     });
-    setTodoLilst(newTodoList);
+    setTodoList(newTodoList);
   };
 
   const onEdit = ({ no, title, done }) => {
@@ -51,7 +63,7 @@ const App = () => {
         newTodoList[idx].title = title;
       }
     });
-    setTodoLilst(newTodoList);
+    setTodoList(newTodoList);
     console.log(newTodoList);
   };
 
